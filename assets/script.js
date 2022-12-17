288994ea2ac49bbe186fd5b8e86d4c711cc8c80d
carsonadair <carsonadairphoto@gmail.com>
2022-12-16 21:49:56 -0500

    honestly i dont know what i did in this one its 10 pm and im tired

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
 var listQuest = document.getElementById('answer');
-var button = document.getElementById('submit');
+var startButton = document.getElementById('submit');
 var questionTitle = document.getElementById('question');
-var questionNum = 0;
+var nextButton = document.getElementById('next');
+var currentQuest = 0
 
 var question1 = {
     question: "Test1",
-    answers: ["1","2","3","4"],
+    answers: ["option","2","3","4"],
     correct: 0
   };
 
@@ -30,13 +31,78 @@ var question4 = {
 var questions = [question1, question2, question3, question4];
 
 function showQuest() {
-  questionTitle.textContent = questions[questionNum].question;
-  listQuest.textContent = questions[questionNum].answers;
+  startButton.classList.add("hide");
+  questionTitle.classList.remove("hide");
+  listQuest.classList.remove("hide");
+  nextButton.classList.remove("hide");
+  nextQuest()
+
+  //questionTitle.textContent = questions[questionNum].question;
+  //listQuest.textContent = questions[questionNum].answers;
+  /* for (var i=0; i<questions[i].answers.length; i++) {
+    var answerBtn = document.createElement('button');
+    answerBtn.textContent = question1.answers[i];
+    listQuest.appendChild(answerBtn);
+  }*/
+
+  /*for (var i=0; i<questions.length; i++) {
+   questionTitle.textContent = questions[i].question;
+    for (var i=0; i<questions[i].answers.length; i++) {
+      var answerBtn = document.createElement('button');
+      answerBtn.textContent = question1.answers[i];
+      listQuest.appendChild(answerBtn);*/
+
+
+      //answerBtn.addEventListener("click", () => {
+         
+      //})
+  }
+
+function nextQuest(){
+  showQuest(currentQuest)
+  nextButton.addEventListener("click",() => {
+
+  });
 }
 
+function showQuest(question){
+  for (var i=0; i<questions.length; i++) {
+    questionTitle.textContent = questions[i].question;
+  }
+}
+
+function startQuiz() {
+  startButton.classList.add("hide");
+  questionTitle.classList.remove("hide");
+  listQuest.classList.remove("hide");
+  nextButton.classList.remove("hide");
+  showQuest();
+  nextQuest();
+
+  //questionTitle.textContent = questions[questionNum].question;
+  //listQuest.textContent = questions[questionNum].answers;
+  /* for (var i=0; i<questions[i].answers.length; i++) {
+    var answerBtn = document.createElement('button');
+    answerBtn.textContent = question1.answers[i];
+    listQuest.appendChild(answerBtn);
+  }*/
+
+  /*for (var i=0; i<questions.length; i++) {
+   questionTitle.textContent = questions[i].question;
+    for (var i=0; i<questions[i].answers.length; i++) {
+      var answerBtn = document.createElement('button');
+      answerBtn.textContent = question1.answers[i];
+      listQuest.appendChild(answerBtn);*/
 
 
-button.addEventListener("click",showQuest);
+      //answerBtn.addEventListener("click", () => {
+         
+      //})
+  }
+
+
+
+startButton.addEventListener("click",startQuiz);
 
 /* Start Timer, display first question with answer choices on the page.
 As a user clicks and answers a question it goes to next question. 
